f4d06748d35021ecb68b52ee4db5947c96ad310b
proman2520 <andrew.prozorovsky@gmail.com>
2023-10-01 21:35:12 -0400

    pre gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -6,7 +6,7 @@ d3.json(url).then(function(data) {
     console.log(data);
 });
 
-//Next step here
+//Define the functions
 function init() {
 
     //Use D3 to select the dropdown menu
@@ -30,8 +30,10 @@ function init() {
 
         //Build the initial plots here
         barChart(default_sample);
+        metadata(default_sample);
+        bubbleChart(default_sample);
     });
-}
+};
 
 function barChart(sample) {
 
@@ -47,7 +49,7 @@ function barChart(sample) {
         //Use otu_ids as the labels for the bar chart.
         let otu_ids = value_data.otu_ids;
 
-        //Use otu_ids as the labels for the bar chart.
+        //Use otu_labels as the hovertext for the chart.
         let otu_labels = value_data.otu_labels;
 
         console.log(otu_ids, otu_labels, sample_values);
@@ -68,23 +70,88 @@ function barChart(sample) {
         Plotly.newPlot("bar", [trace]);
 
     })
-}
+};
 
-function bubbleChart() {
+function bubbleChart(sample) {
 
-}
+    //Retrieve data with D3
+    d3.json(url).then((data) => {
+        let sample_data = data.samples;
+        let value = sample_data.filter(result => result.id == sample);
+        let value_data = value[0];
+        
+        //Use sample_values for the y values and marker size.
+        let sample_values = value_data.sample_values;
 
-function metadata() {
+        //Use otu_ids for the x values and marker colors.
+        let otu_ids = value_data.otu_ids;
 
-}
+        //Use otu_labels for the text values.
+        let otu_labels = value_data.otu_labels;
+
+        console.log(otu_ids, otu_labels, sample_values);
+
+        let trace = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+
+        let layout = {
+            xaxis: {title: "OTU ID"}
+        };
+
+        Plotly.newPlot("bubble", [trace], layout);
+
+    })
+};
+
+function metadata(sample) {
+
+    //Retrieve data with D3
+    d3.json(url).then((data) => {
+
+        let metadata = data.metadata;
+
+        //Filter on selected sample
+        let value = metadata.filter(result => result.id == sample);
+        console.log(value);
+
+        //Clean up the metadata
+        let value_data = value[0];
+        d3.select("#sample-metadata").html("");
+
+        //Retrieve and populate metadata
+        Object.entries(value_data).forEach(([key, value]) => {
+            console.log(key, value);
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+        });
+
+    });
+
+};
 
 // function gaugeChart() {
     //Put in bonus.js
 // }
 
-function changeSample() {
+function optionChanged(new_sample) {
 
-}
+    //Log new value
+    console.log(new_sample);
+
+    //Call all functions
+    barChart(new_sample);
+    metadata(new_sample);
+    bubbleChart(new_sample);
+
+};
 
 //Call the initialize function
 init();
